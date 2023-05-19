5970706f29f418f6ec9c3b2b3d0ec2d208549c70
Cho-woo-pil <jwp4465@naver.com>
2023-05-19 14:49:07 +0900

    refactor : root폴더 생성

---
+++ src/main/java/com/hanghae7/alcoholcommunity/domain/common/config/exampleConfig.java
@@ -0,0 +1,11 @@
+package com.hanghae7.alcoholcommunity.domain.common.config;
+
+/**
+ * Please explain the class!!
+ *
+ * @fileName      : exampleConfig
+ * @author        : mycom
+ * @since         : 2023-05-19
+ */
+public class exampleConfig {
+}
---
+++ src/main/java/com/hanghae7/alcoholcommunity/domain/member/controller/example.java
@@ -0,0 +1,11 @@
+package com.hanghae7.alcoholcommunity.domain.member.controller;
+
+/**
+ * Please explain the class!!
+ *
+ * @fileName      : example
+ * @author        : mycom
+ * @since         : 2023-05-19
+ */
+public class example {
+}
