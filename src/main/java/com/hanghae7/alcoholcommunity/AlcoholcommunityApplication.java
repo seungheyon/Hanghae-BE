9d44f1a297cf4cfc7b8d62a5c28532f68624d39c
Cho-woo-pil <jwp4465@naver.com>
2023-05-19 11:39:43 +0900

    Initial commit

---
+++ src/main/java/com/hanghae7/alcoholcommunity/AlcoholcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae7.alcoholcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcoholcommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanghae7/alcoholcommunity/AlcoholcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae7.alcoholcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholcommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
