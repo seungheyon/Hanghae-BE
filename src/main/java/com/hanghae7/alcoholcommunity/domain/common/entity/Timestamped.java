02ab49f50d97ffc086e4b022af11980205630e6d
choihana <chhana0703@gmail.com>
2023-05-22 22:41:15 +0900

    feat:add Timestamped

---
+++ src/main/java/com/hanghae7/alcoholcommunity/domain/common/entity/Timestamped.java
@@ -0,0 +1,39 @@
+package com.hanghae7.alcoholcommunity.domain.common.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jdk8.LongStreamSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass // 멤버 변수가 컬럼이 되도록 설정
+@EntityListeners(AuditingEntityListener.class) // 변경되었을 때 자동으로 기록
+
+public abstract class Timestamped {
+
+    @CreatedDate // 최초 생성 시점 기록
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonSerialize(using = LongStreamSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 마지막 변경 시점 기록
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDateTime modifiedAt;
+
+}
