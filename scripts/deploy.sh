e95a06da53ea8d93a4674619c0fd8f27d13ee23a
JayB202 <122235186+JayB202@users.noreply.github.com>
2023-06-03 16:44:56 +0900

    test:  sulma....

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,13 +10,14 @@ sudo chmod +x $JAR_PATH
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z "$CURRENT_PID" ]
+if [ -z "$CURRENT_PID" ];
 then
   echo "> 종료할것 없음."
 else
   echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
+
 fi
 
 echo "> $JAR_PATH 배포"
