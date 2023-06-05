b0d313852e8c35e17f16096b86cda76688288fa5
choihana <chhana0703@gmail.com>
2023-06-05 17:24:31 +0900

    fix:CICD를 위한 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,17 +6,16 @@ cd $REPOSITORY
 
 APP_NAME=soolo-serv
 
-JAR_NAME=$(ls $REPOSITORY/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-JAR_PATH=$REPOSITORY/$JAR_NAME
-
-JAR 파일에 실행 권한 설정
-sudo chmod +x $JAR_PATH
-sudo chmod +w $JAR_PATH
+# JAR 파일에 실행 권한 설정
+sudo chmod +x "$JAR_PATH"
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z "$CURRENT_PID" ]; then
+if [ -z $CURRENT_PID ]
+then
   echo "> 종료할것 없음."
 else
   echo "> kill -9 $CURRENT_PID"
@@ -24,11 +23,5 @@ else
   sleep 5
 fi
 
-sudo chmod +x $JAR_NAME
 echo "> $JAR_PATH 배포"
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
-
-# 스크립트 종료 후 변경 사항이 저장되는지 확인
-sleep 10  # 충분한 대기 시간을 주어 변경 사항 저장 확인
-
-echo "> 배포 후 변경 사항 저장 완료"
